b4f8c083211e335848087718bfc11f6da9c14b1d
Lyubomir Cholakov <l.cholakoff@gmail.com>
2022-09-19 09:29:21 +0200

    Fill cells with random color. Darken cell with each mouse hover. All requirements met.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Etch a Sketch assignment from The Odin Project. 
 Features: 
  - default 16x16 grid for drawing
- - user can choose grid size, max 100 squares / side
+ - user can choose grid size, max 48 squares / side (reduced from 100 due to lag)
  - on mouse hover change color
  - assign a random color that gets 10% darker with each mouse hover
--- script.js
+++ script.js
@@ -9,30 +9,54 @@ updateGrid(gridSizeSlider.value)
 function updateGrid(val) {
     gridSizeText.textContent = `Grid size: ${val}`;
 
-    // TODO clear or child elements
     while (gridContainer.firstChild) {
-        console.log("enter while");
         gridContainer.removeChild(gridContainer.firstChild);
-        console.log("exit while");
     }
 
     for(let i = 1; i <= gridSizeSlider.value**2; ++i) {
-
         console.log("gridsize", gridSizeSlider.value);
         gridContainer.style.setProperty('grid-template-columns', `repeat(${gridSizeSlider.value}, 1fr)`);
         const newDiv = document.createElement('div');
         newDiv.style.setProperty('border', '0px inset blue');
         newDiv.style.setProperty('background-color', 'white');
-        // newDiv.style.setProperty('width', '10');
-        // newDiv.style.setProperty('height', '10');
-        // newDiv.insertAdjacentText('beforeend', i);
         gridContainer.appendChild(newDiv);
     }
 
     for(let i = 0; i < gridContainer.children.length; ++i)
     {
-        gridContainer.children[i].addEventListener('mouseenter', (event) => {gridContainer.children[i].style.setProperty('background-color', 'black');});
+        gridContainer.children[i].addEventListener('mouseenter', (event) => {setCellColor(gridContainer.children[i])});
     }
 }
 
+function setCellColor(cell)
+{
+    currCellStyle = getComputedStyle(cell).backgroundColor;
+    if (currCellStyle === 'rgb(255, 255, 255)')
+    {
+        cell.style.setProperty('background-color', getRandColor());
+    } else
+    {
+        cell.style.setProperty('background-color', RGB_Linear_Shade(-.1, currCellStyle));
+    }
+    
+}
 
+function getRandColor()
+{
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function RGB_Linear_Shade(p,col)
+{
+    let i=parseInt;
+    let r=Math.round;
+    let [a,b,c,d]=col.split(",");
+    let P=p<0;
+    let t=P?0:255*p;
+    P=P?1+p:1-p;
+    return"rgb"+(d?"a(":"(")+r(i(a[3]=="a"?a.slice(5):a.slice(4))*P+t)+","+r(i(b)*P+t)+","+r(i(c)*P+t)+(d?","+d:")");
+}
